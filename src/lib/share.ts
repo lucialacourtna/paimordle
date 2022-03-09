43622edb6760c205a285b9eeecada4239ddb33cf
lucialacourtna <78623423+lucialacourtna@users.noreply.github.com>
2022-03-09 15:55:47 -0500

    Update share.ts

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -1,32 +1,84 @@
 import { getGuessStatuses } from './statuses'
-import { solutionIndex } from './words'
+import { solutionIndex, unicodeSplit } from './words'
 import { GAME_TITLE } from '../constants/strings'
+import { MAX_CHALLENGES } from '../constants/settings'
+import { UAParser } from 'ua-parser-js'
 
-export const shareStatus = (guesses: string[], lost: boolean) => {
-  navigator.clipboard.writeText(
-    `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
-      generateEmojiGrid(guesses) +
-      '\n\nhttps://paimordle.vercel.app/'
-  )
+const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable']
+const parser = new UAParser()
+const browser = parser.getBrowser()
+const device = parser.getDevice()
+
+export const shareStatus = (
+  guesses: string[],
+  lost: boolean,
+  isHardMode: boolean,
+  isDarkMode: boolean,
+  isHighContrastMode: boolean,
+  handleShareToClipboard: () => void
+) => {
+  const textToShare =
+    `${GAME_TITLE} ${solutionIndex} ${
+      lost ? 'X' : guesses.length
+    }/${MAX_CHALLENGES}${isHardMode ? '*' : ''}\n\n` +
+    generateEmojiGrid(guesses, getEmojiTiles(isDarkMode, isHighContrastMode))
+
+  const shareData = { text: textToShare }
+
+  let shareSuccess = false
+
+  try {
+    if (attemptShare(shareData)) {
+      navigator.share(shareData)
+      shareSuccess = true
+    }
+  } catch (error) {
+    shareSuccess = false
+  }
+
+  if (!shareSuccess) {
+    navigator.clipboard.writeText(textToShare)
+    handleShareToClipboard()
+  }
 }
 
-export const generateEmojiGrid = (guesses: string[]) => {
+export const generateEmojiGrid = (guesses: string[], tiles: string[]) => {
   return guesses
     .map((guess) => {
       const status = getGuessStatuses(guess)
-      return guess
-        .split('')
+      const splitGuess = unicodeSplit(guess)
+
+      return splitGuess
         .map((_, i) => {
           switch (status[i]) {
             case 'correct':
-              return '🟩'
+              return tiles[0]
             case 'present':
-              return '🟨'
+              return tiles[1]
             default:
-              return '⬜'
+              return tiles[2]
           }
         })
         .join('')
     })
     .join('\n')
+}
+
+const attemptShare = (shareData: object) => {
+  return (
+    // Deliberately exclude Firefox Mobile, because its Web Share API isn't working correctly
+    browser.name?.toUpperCase().indexOf('FIREFOX') === -1 &&
+    webShareApiDeviceTypes.indexOf(device.type ?? '') !== -1 &&
+    navigator.canShare &&
+    navigator.canShare(shareData) &&
+    navigator.share
+  )
+}
+
+const getEmojiTiles = (isDarkMode: boolean, isHighContrastMode: boolean) => {
+  let tiles: string[] = []
+  tiles.push(isHighContrastMode ? '🟧' : '🟩')
+  tiles.push(isHighContrastMode ? '🟦' : '🟨')
+  tiles.push(isDarkMode ? '⬛' : '⬜')
+  return tiles
 }
