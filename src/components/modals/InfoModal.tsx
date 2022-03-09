ee8db3b6446924f86f8e9ed7b8f6cef3e67555c8
lucialacourtna <78623423+lucialacourtna@users.noreply.github.com>
2022-03-09 11:18:57 -0500

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,7 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
+        Guess the word in 6 tries. Words can be characters, items, and concepts 
+        relating to Genshin. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
 
@@ -45,6 +46,28 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter K is not in the word in any spot.
+      </p>
+      
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        This is an open source version of the word guessing game we all know and
+        love -{' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+          check out the code here
+        </a>{' '}
+        Modified by {' '}
+        <a
+          href="https://twitter.com/CYBERCH1CK"
+          className="underline font-bold"
+        >
+          CYBERCH1CK
+        </a>{' '}
+        Find updates on the latest with {' '}
+        <a href="https://twitter.com/paimordle" className="underline font-bold">
+          Paimordle
+        </a>{' '}
       </p>
     </BaseModal>
   )
